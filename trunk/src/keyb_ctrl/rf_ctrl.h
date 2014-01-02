088e7caba23924b5a42e86d924bd247eb78bb134
ferenc.szili@gmail.com <ferenc.szili@gmail.com@2f2a0e67-9daf-876d-ef88-28c39feb6a9e>
2014-01-02 09:06:28 +0000

    added counting RF packets (total/retransmit/lost) to menu

--- trunk/src/keyb_ctrl/rf_ctrl.h
+++ trunk/src/keyb_ctrl/rf_ctrl.h
@@ -1,13 +1,14 @@
 #pragma once
 
+// stat counters
+extern uint32_t plos_total, arc_total, rf_packets_total;
+
 void rf_ctrl_init(void);
 
 // LED status will be set to LED_STATUS_NOT_RECEIVED if no status has been received
 #define LED_STATUS_NOT_RECEIVED	0xff
 
 bool rf_ctrl_send_message(const void* buff, const uint8_t num_bytes);
-
-void rf_ctrl_get_observe(uint8_t* arc, uint8_t* plos);
 
 uint8_t rf_ctrl_read_ack_payload(void* buff, const uint8_t buff_size);
 
